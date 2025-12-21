2b7689dddb29b1a34f21e08fdb5c9a7efb1a17d6
udayrshankar <138763436+udayrshankar@users.noreply.github.com>
2025-12-21 13:54:02 +0530

    posthog intialization

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,26 @@ import { TransitionProvider } from "./components/TransitionContext";
 import App from "./App";
 import "./index.css";
 
+// 1. Import PostHog
+import posthog from 'posthog-js';
+import { PostHogProvider } from '@posthog/react';
+
+// 2. Initialize PostHog
+posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+  person_profiles: 'identified_only', // Recommended for better privacy/performance
+  // capture_pageview: false // Uncomment this if you want to manually control page views
+});
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <TransitionProvider>
-        <App />
-      </TransitionProvider>
-    </BrowserRouter>
+    {/* 3. Wrap app in PostHogProvider */}
+    <PostHogProvider client={posthog}>
+      <BrowserRouter>
+        <TransitionProvider>
+          <App />
+        </TransitionProvider>
+      </BrowserRouter>
+    </PostHogProvider>
   </React.StrictMode>
 );
